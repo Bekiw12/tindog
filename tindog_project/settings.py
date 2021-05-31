4b5b0665078f58eb87c05a0019320c7552992f4b
Bekzod <bekiwnamazov12@gmail.com>
2021-05-31 20:03:02 +0500

    settings fixed

--- tindog_project/settings.py
+++ tindog_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u1q88o$57!6l%+@o$)vf79avzf76u5ojdx@fs&*l69-a0rd3o3'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
