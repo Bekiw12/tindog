7861ed60715bc38b0a6133cc0d1bb068b5d90f2e
Bekzod <bekiwnamazov12@gmail.com>
2021-05-31 19:45:04 +0500

    settings fixed

--- tindog_project/settings.py
+++ tindog_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u1q88o$57!6l%+@o$)vf79avzf76u5ojdx@fs&*l69-a0rd3o3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tindoguzb.herokuapp.com']
 
